e609d15c174168cd728922e857a80841c16f92a8
AshikSazal <sojolislm@gmail.com>
2024-07-17 16:05:35 +0600

    fix the login issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,12 +27,6 @@ class UserController extends Controller
 
         Auth::login($user);
 
-        if (Session::has('oldUrl')) {
-            $oldUrl = Session::get('oldUrl');
-            Session::forget('oldUrl');
-            return redirect()->to($oldUrl);
-        }
-
         return redirect()->route('user.profile');
     }
 
@@ -47,11 +41,6 @@ class UserController extends Controller
         ]);
 
         if(Auth::attempt(['email'=>$request->input('email'),  'password'=>$request->input('password')])){
-            if (Session::has('oldUrl')) {
-                $oldUrl = Session::get('oldUrl');
-                Session::forget('oldUrl');
-                return redirect()->to($oldUrl);
-            }
             return redirect()->route('user.profile');
         }
         return redirect()->back();
